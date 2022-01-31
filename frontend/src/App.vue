feb94ae18b86cd5640761672753a7e78536eadd1
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2022-01-31 23:35:58 +0700

    removed web3, got web3modal vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,8 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Web3 from 'web3';
-// import provider from 'web3/types'
 
 export default defineComponent({
   name: 'App',
-  setup() {
-    const web3 = new Web3();
-
-    console.log(web3);
-  }
 })
 </script>
