2e5d734e2de8acef064265ed200d980eeea5bba1
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2022-02-02 13:26:40 +0700

    correct implementation of disconnect/connect wallet

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,62 +1,78 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-
     <q-header>
       <q-toolbar>
         <q-toolbar-title>
           <q-icon class="text-dark" size="lg">
             <img src="~assets/docsignlogo.svg" />
-          </q-icon> <span class="gt-xs">Doc Sign</span>
+          </q-icon>
+          <span class="gt-xs">Doc Sign</span>
         </q-toolbar-title>
 
-        <q-btn flat round dense icon="notifications" class="q-mr-xs" >
+        <q-btn flat round dense icon="notifications" class="q-mr-xs">
           <q-badge color="accent" floating>4</q-badge>
         </q-btn>
         <q-btn flat round dense icon="settings" class="q-mr-xs" />
         <div v-if="account">{{ account }}</div>
-        <q-btn v-else unelevated rounded color="secondary" label="Connect Wallet" class="q-mx-md" @click="connectWallet" />
+        <q-btn
+          v-else
+          unelevated
+          rounded
+          color="secondary"
+          label="Connect Wallet"
+          class="q-mx-md"
+          @click="getAccount"
+        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
-        <q-toolbar>
-          <q-tabs
-              class="absolute-center"
-            >
-              <q-route-tab label="Documents" to="/documents" exact active />
-              <q-route-tab label="Signs" to="/signs" exact />
-          </q-tabs>
-        </q-toolbar>
+      <q-toolbar>
+        <q-tabs class="absolute-center">
+          <q-route-tab label="Documents" to="/documents" exact active />
+          <q-route-tab label="Signs" to="/signs" exact />
+        </q-tabs>
+      </q-toolbar>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script lang="ts">
-import { useMetamask } from 'src/core/composables/metamask';
-import { defineComponent, ref } from 'vue'
+import { useWeb3Provider } from "src/core/composables/web3provider";
+import { defineComponent, onMounted, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
-  setup () {
-    const { getAccount } = useMetamask();
-    
-    const account = ref();
+  name: "MainLayout",
+  setup() {
+    const account = ref("");
 
-    const connectWallet = () => {
-      getAccount().then((result) => {
-        account.value = result
-      }).catch((error) => {
-        console.log(error);
-      })
-    }
+    onMounted(() => {
+      const ethereum = window.ethereum;
+      if (typeof ethereum !== "undefined") {
+        ethereum.on("accountsChanged", function (accounts) {
+          let accountArr = accounts as string[];
+          if (accountArr.length > 0) {
+            account.value = accountArr[0];
+          } else {
+            account.value = "";
+          }
+        });
+
+        ethereum.on("connect", function (connectInfo) {
+          console.log(connectInfo);
+        });
+
+        if (ethereum.isConnected()) {
+          account.value = ethereum.selectedAddress ?? "";
+        }
+      }
+    });
 
     return {
       account,
-      connectWallet
-    }
-  }
-})
+      ...useWeb3Provider(),
+    };
+  },
+});
 </script>
-
