08bd0b04a667bb6ea5838d19291494bda3225763
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2022-08-10 07:52:12 +0200

    Delete ds-frontend directory

--- ds-frontend/src/i18n/index.js
+++
@@ -1,7 +0,0 @@
-import enUS from './en-US';
-import deDE from './de-DE';
-
-export default {
-  'en-US': enUS,
-  'de-DE': deDE,
-};
--- ds-frontend/src/pages/main-tabs.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <q-tabs class="absolute-center">
-    <q-route-tab label="Documents" to="/documents" exact active />
-    <q-route-tab label="Signs" to="/signs" exact />
-    <q-route-tab label="Playground" to="/playground" exact />
-  </q-tabs>
-</template>
